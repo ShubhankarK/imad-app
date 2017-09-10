6224c7cf511ce779452dae011893c86ce499c47d
ShubhankarK <shubhankarkalekarsk@gmail.com>
2017-09-10 11:24:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'shubhankarkalekarsk',
+    database: 'shubhankarkalekarsk',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.emv.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -76,9 +85,18 @@ app.get('/article-three', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
+var pool = new Pool(config);
 apt.get('/test-db', function (req,res){
     //make a select request
     //return a response with the results
+    pool.query('SELECT * from test', function(err,result) {
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    };
 });
 
 
